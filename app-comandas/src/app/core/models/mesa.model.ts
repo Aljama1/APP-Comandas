db9bd003e413c1be52d8dd5f4feb9daf7dc5c1e5
Manuel <manuelaljama9@gmail.com>
2026-05-10 19:51:56 +0200

    refactor: eliminar archivos y componentes sin usar (limpieza final)

--- app-comandas/src/app/core/models/mesa.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Mesa {
-  id: string;                  // ID único de la mesa en la base de datos
-  numeroMesa: number;          // Número visible de la mesa (Ej: Mesa 4)
-  tokenQR: string;             // Token o Hash secreto del QR asociado a esta mesa
-  capacidad: number;           // Capacidad máxima de comensales
-  estaOcupada: boolean;        // Estado actual de la mesa
-  idClienteActual?: string;    // Si está ocupada, UID del usuario/cliente vinculado a la sesión actual
-}
